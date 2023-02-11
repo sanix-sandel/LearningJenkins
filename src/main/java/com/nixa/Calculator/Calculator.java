d7843859ede9dabc63f421f6a20c842203cd95ec
Sanix <sanick.ikani@rakuten.com>
2023-02-11 09:34:04 +0900

    polling SCM

--- src/main/java/com/nixa/Calculator/Calculator.java
+++ src/main/java/com/nixa/Calculator/Calculator.java
@@ -2,9 +2,16 @@ package com.nixa.Calculator;
 
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class Calculator {
 
+    /**
+     *
+     * @param a
+     * @param b
+     * @return
+     */
     public int sum(int a, int b){
         return a+b;
     }
